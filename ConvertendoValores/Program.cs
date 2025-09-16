8646c4befd751da5a024a343dc3837278c69fc11
RianAFK <rian.colombaro@aluno.senai.br>
2025-09-16 15:08:59 -0300

    Variaveis

---
+++ ConvertendoValores/Program.cs
@@ -0,0 +1,18 @@
+﻿string nome;
+int idade;
+char sexo;
+double salario;
+
+Console.WriteLine("Digite seu nome:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade:");
+idade = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Qual é o seu sexo: M ou F");
+sexo = Convert.ToChar(Console.ReadLine().ToUpper());
+
+Console.WriteLine("Digite seu salário:");
+salario = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"O seu nome é {nome}, você tem {idade} anos, seu sexo é {sexo} e seu salário é: R${salario},00 ");
---
+++ OperadoresAritmeticos/Program.cs
@@ -0,0 +1,31 @@
+﻿// Operadores Aritméticos
+
+/*
+    + = Adição
+    - = Subtração
+    * = Multiplicação
+    / = Divisão
+*/
+
+// Entrada dos dados
+int soma = 2 + 2;
+int subtracao = 10 - 5;
+int multiplicacao = 3 * 7;
+double divisao = 3 / 2.5;
+
+// Saída dos dados
+Console.WriteLine($"2 + 2 = {soma}");
+Console.WriteLine($"10 - 5 = {subtracao}");
+Console.WriteLine($"3 * 7 = {multiplicacao}");
+Console.WriteLine($"3 / 2.5 = {divisao}");
+
+//Utilizando Operadores Aritméticos com variáveis
+int num1, num2, resultado;
+Console.WriteLine("Insira a primeira Nota");
+num1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Insira a segunda Nota");
+num2 = Convert.ToInt16(Console.ReadLine());
+
+resultado = num1 + num2;
+Console.WriteLine($"A soma das notas é: {resultado}");
